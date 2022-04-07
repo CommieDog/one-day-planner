478b5f963b1ba173c35996a677b795f943c22925
John Netzel <johnrnetzel@gmail.com>
2022-04-06 17:03:12 -0800

    Add textareas to timeblock elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,11 +8,13 @@ function createTimeblocks()
     for(var i = 9; i <= 17; i++) // Iterate through the hours of the workday
     {
         var timeblock = $("<div>"); // Create new <div>
-        var timeblockP = $("<p>");
-        timeblockP.text(i);
-        timeblock.append(timeblockP);
+        timeblock.addClass("time-block");
+        timeblock.data("startingHour", i);
         timeblocksEl.append(timeblock);
     }
+    var timeblocks = $(".time-block"); // Select all timeblocks
+    timeblocks.append($("<p>").text("A"));
+    timeblocks.append($("<textarea>"));
 }
 
 createTimeblocks();
