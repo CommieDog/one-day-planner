489c86cb2ad5bd6616364a974b0b22d649969896
John Netzel <johnrnetzel@gmail.com>
2022-04-06 20:15:28 -0800

    Add proper class to timeblock textareas

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,18 +13,40 @@ function createTimeblocks()
         timeblocksEl.append(timeblock);
     }
     var timeblocks = $(".time-block"); // Select all timeblocks
-    timeblocks.append($("<p>").addClass("col-1").text("A"));
+    timeblocks.append($("<p>").addClass("col-1 hour").text("A"));
     timeblocks.append($("<textarea>").addClass("col-10"));
     timeblocks.append($("<button>").addClass("col-1 saveBtn").text("Save"));
 
     // Set the proper time value for each timeblock
-    $(".time-block p").each(function(_i, el)
+    $(".hour").each(function(_i, el)
     {
         el = $(el);
         var timeLabel = el.parent().data("startingHour");
         timeLabel = moment().startOf("day").add(timeLabel, "hours");
         timeLabel = timeLabel.format("h A");
         el.text(timeLabel);
+    });
+
+    var currentHour = moment().startOf("hour")
+    // Set the proper time class value for each timeblock
+    $(".time-block textarea").each(function(_i, el)
+    {
+        el = $(el);
+        var timeValue = el.parent().data("startingHour");
+        timeValue = moment().startOf("day").add(timeValue, "hours");
+        if(currentHour.isAfter(timeValue))
+        {
+            timeValue = "past";
+        }
+        else if(currentHour.isBefore(timeValue))
+        {
+            timeValue = "future";
+        }
+        else
+        {
+            timeValue = "present";
+        }
+        el.addClass(timeValue);
     });
 }
 
