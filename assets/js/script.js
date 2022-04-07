f875ba469e502bdb4a5be2347b3b3ce990a94602
John Netzel <johnrnetzel@gmail.com>
2022-04-06 17:08:42 -0800

    Fix row layout of timeblocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,13 +8,14 @@ function createTimeblocks()
     for(var i = 9; i <= 17; i++) // Iterate through the hours of the workday
     {
         var timeblock = $("<div>"); // Create new <div>
-        timeblock.addClass("time-block");
+        timeblock.addClass("row time-block");
         timeblock.data("startingHour", i);
         timeblocksEl.append(timeblock);
     }
     var timeblocks = $(".time-block"); // Select all timeblocks
-    timeblocks.append($("<p>").text("A"));
-    timeblocks.append($("<textarea>"));
+    timeblocks.append($("<p>").addClass("col-1").text("A"));
+    timeblocks.append($("<textarea>").addClass("col-10"));
+    timeblocks.append($("<button>").addClass("col-1 saveBtn").text("Save"));
 }
 
 createTimeblocks();
