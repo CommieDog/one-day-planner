4b1a3221f55549128d886b7c011fce9ebbcf591e
John Netzel <johnrnetzel@gmail.com>
2022-04-06 16:39:13 -0800

    Add timeblock <div> elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
 const currentDayEl = $("#currentDay");
+const timeblocksEl = $("#timeblocks");
 
 currentDayEl.text(moment().format("dddd, MMMM Do, YYYY"));
+
+function createTimeblocks()
+{
+    for(var i = 9; i <= 17; i++) // Iterate through the hours of the workday
+    {
+        var timeblock = $("<div>"); // Create new <div>
+        var timeblockP = $("<p>");
+        timeblockP.text(i);
+        timeblock.append(timeblockP);
+        timeblocksEl.append(timeblock);
+    }
+}
+
+createTimeblocks();
